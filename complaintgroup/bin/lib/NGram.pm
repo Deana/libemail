077c106c3d69c2f339a28d360ca4e2ccb0ed3967
Peter Blair <petermblair@gmail.com>
2011-10-04 13:55:30 -0400

    Stuff

--- complaintgroup/bin/lib/NGram.pm
+++ complaintgroup/bin/lib/NGram.pm
@@ -5,5 +5,41 @@ use ComplaintDB;
 
 use Word;
 
+sub get( $$ ){
+	my ($self, $ngram) = @_;
+
+	my $W = Word->new->copy_into( $self->copy );
+
+	my @b;
+
+	if( $ngram =~ /\d+(?::\d+)?/ ){
+		my @a = split /:/, $ngram ;
+
+		for ( @a ){
+			push( @b, $W->get( $_ ) );
+		}
+
+	} else { die ("Unsupported input:[$ngram]\n"); }
+	return \@b;
+
+}
+
+sub create_ngram( $$ ){
+	my ( $self, $a_ref ) = @_;
+	
+	my $W = Word->new;
+	$W->copy_into( $self->copy() );
+
+	my @a;
+
+	# Iterate over the array, creating words by these,
+	for my $word ( @$a_ref ){
+		my $id = $W->get( $word );
+		push( @a, $id );
+	}
+
+	my @ngram = join( ":", @a );
+	return \@ngram;
+}
 
 1;
